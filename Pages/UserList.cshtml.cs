e1c8adf53b0d3a02f3d96d1a94ad0634bd96f30b
andorgast <andorgast@gmail.com>
2026-03-25 11:21:44 +0100

    group creation and group adding adding

--- Pages/UserList.cshtml.cs
+++ Pages/UserList.cshtml.cs
@@ -21,20 +21,40 @@ namespace Vocas.Pages
             }
             return KD;
         }
-        public List<UserViewModel> Users { get; private set; } = [];
-        public void OnGet()
+        public void createGroup(int UserToAdd)
         {
+            var CreatedGroup = new GroupViewModel(new UserViewModel(UserToAdd), new UserViewModel(currentUserId));
+            RedirectToPage("/group", new { id = CreatedGroup.GroupId});
+        }
+        public int currentUserId { get; private set; } = 7;
+        public List<UserViewModel> Users { get; private set; } = [];
+        public PageResult OnGet(int? groupCreate)
+        {
+            if (groupCreate != null)
+            {
+                if ((int)groupCreate != currentUserId)
+                {
+                    createGroup((int)groupCreate);
+                }    
+            }
             var conn = new MySqlConnection(connectionString);
             conn.Open();
             var cmd = new MySqlCommand(
-                @"SELECT * FROM users", conn
+                @"SELECT * FROM users WHERE NOT id=@id", conn
             );
+            cmd.Parameters.AddWithValue("@id", currentUserId);
             var reader = cmd.ExecuteReader();
+            if (reader.IsClosed)
+            {
+                conn.Close();
+                return Page();
+            }
             while (reader.Read()) 
             {
-                Users.Add(new UserViewModel(reader.GetInt32(0), reader.GetString(1), reader.GetInt32(3), reader.GetInt32(4), reader.GetInt32(5), reader.GetString(6), [reader.GetString(7)]));
+                Users.Add(new UserViewModel(reader.GetInt32(0), reader.GetString(1), reader.GetInt32(3), reader.GetInt32(4), reader.GetInt32(5), reader.GetString(6), reader.GetString(7).Replace("\n","")));
             }
             conn.Close();
+            return Page();
         }
     }
 }
